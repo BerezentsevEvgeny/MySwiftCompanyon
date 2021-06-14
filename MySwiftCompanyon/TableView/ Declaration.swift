eed364ab42644cadb855f746ccbf015fbe741fc0
Berezentsev Evgeny <jonn09@icloud.com>
2021-06-14 16:09:55 +0300

    Some

---
+++ MySwiftCompanyon/TableView/ Declaration.swift
@@ -0,0 +1,47 @@
+
+import Foundation
+import UIKit
+
+class TableViewConroller: UIViewController, UITableViewDataSource {
+    
+    // Model
+    struct Person {
+        let name: String
+    }
+    
+    private var models = [Person]()
+    
+    private let tableView: UITableView = {
+        let table = UITableView()
+        table.register(UITableViewCell.self, forCellReuseIdentifier: "cell")
+        return table
+    }()
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        tableView.addSubview(tableView)
+        tableView.frame = view.bounds
+        tableView.dataSource = self
+    }
+    
+    // Confugure models
+    private func configureModels() {
+        let names = ["Joe","Sam"]
+        
+        for name in names {
+            models.append(Person(name: name))
+        }
+    }
+    
+    
+    // Delegate methods
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        1
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "cell", for: indexPath)
+        return cell
+    }
+    
+}
