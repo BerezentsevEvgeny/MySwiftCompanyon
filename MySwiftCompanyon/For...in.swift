22ad0fa6b74f4d2e4fe71778b6894b5e60540c28
Berezentsev Evgeny <jonn09@icloud.com>
2021-06-16 21:28:36 +0300

    Some corrections

---
+++ MySwiftCompanyon/For...in.swift
@@ -0,0 +1,42 @@
+//
+//  For...in.swift
+//  MySwiftCompanyon
+//
+//  Created by Евгений Березенцев on 13.06.2021.
+//
+
+import Foundation
+
+func a() {
+    
+    // Использование итераций
+    for iteration in  1...10 {
+
+        if Int.random(in: 1...10) == 5 {
+            print("Что бы выпало число 5 понадобилось \(iteration) итераций")
+            break
+        }
+    }
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+
+}
+
