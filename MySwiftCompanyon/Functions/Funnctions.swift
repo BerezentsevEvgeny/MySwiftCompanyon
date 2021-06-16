6bf79222d85aa1263a99de991d924aab6d2b85ed
Berezentsev Evgeny <jonn09@icloud.com>
2021-06-16 21:37:18 +0300

    A

--- MySwiftCompanyon/Functions/Funnctions.swift
+++ MySwiftCompanyon/Functions/Funnctions.swift
@@ -1,2 +1,16 @@
 
 import Foundation
+
+private func addNumbers(argument parameter: Int){ }
+
+//если код в одну строку можно не ставить return
+
+// let result = addNumbers() вызывает функцию
+// let result = addNumbers  создает копию
+
+//при вызове:
+//параметр - внутренний  - используется внутри функции
+//аргумент - внешний     -  для связки
+// _n :  пустой
+
+//Вариативные параметры -  должен быть один параметр (_ numbers: Double...)
