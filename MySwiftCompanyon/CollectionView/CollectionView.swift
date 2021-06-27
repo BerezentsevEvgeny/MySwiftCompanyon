cb19886c54de5a7840bcf7c302126c202c5c29b6
Berezentsev Evgeny <jonn09@icloud.com>
2021-06-27 12:58:21 +0300

    a

---
+++ MySwiftCompanyon/CollectionView/CollectionView.swift
@@ -0,0 +1,26 @@
+
+import Foundation
+
+let Collection = """
+        
+        Создание кодом
+        
+        private var collectionView: UICollectionView?
+    
+        override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        collectionView = UICollectionView(frame: .zero, collectionViewLayout: configureLayout())
+        
+        guard let collectionView = collectionView else { return }
+        
+        collectionView.register(CustomCollectionViewCell.self, forCellWithReuseIdentifier: CustomCollectionViewCell.identifier)
+        
+        collectionView.dataSource = self
+        collectionView.delegate = self
+        view.addSubview(collectionView)
+        collectionView.frame = view.bounds
+        collectionView.backgroundColor = .white
+        
+        
+        """
