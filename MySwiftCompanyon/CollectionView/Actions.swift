babd3234166e53d83c8b156d94edf8574145a331
Berezentsev Evgeny <jonn09@icloud.com>
2021-07-03 17:55:57 +0300

    a

---
+++ MySwiftCompanyon/CollectionView/Actions.swift
@@ -0,0 +1,10 @@
+
+import Foundation
+
+
+// Действие по нажатию кнопки
+button.addTarget(self, action: #selector(buttonTapped), for: .touchUpInside)
+
+@objc private func buttonTapped() {
+//    действие
+}
