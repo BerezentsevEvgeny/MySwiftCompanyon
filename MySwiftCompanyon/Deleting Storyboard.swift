1f31656ed2d9a18bbaabf4dfc76d198bb97b362c
Berezentsev Evgeny <jonn09@icloud.com>
2021-07-26 16:35:11 +0300

    a

---
+++ MySwiftCompanyon/Deleting Storyboard.swift
@@ -0,0 +1,44 @@
+//
+//  Deleting Storyboard.swift
+//  MySwiftCompanyon
+//
+//  Created by Евгений Березенцев on 26.07.2021.
+//
+
+import UIKit
+
+// New versions
+// Удаляем вкладку MainStoryboard и сам файл, в info.plist -> ApplicationSceneManifest удалить StoryboardName
+
+class SceneDelegatE: UIResponder, UIWindowSceneDelegate {
+
+    var window: UIWindow?
+
+    func scene(_ scene: UIScene, willConnectTo session: UISceneSession, options connectionOptions: UIScene.ConnectionOptions) {
+
+        guard let windowScene = (scene as? UIWindowScene) else { return }
+        let window = UIWindow(windowScene: windowScene)
+        window.rootViewController = UIViewController()
+        window.makeKeyAndVisible()
+        self.window = window
+    }
+}
+
+
+// Old versions
+//Удаляем SceneDelegate и ApplicationSceneManifest в info.plist
+
+class AppDelegatE: UIResponder, UIApplicationDelegate {
+    
+    var window: UIWindow?
+
+    func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
+        
+        let window = UIWindow(frame: UIScreen.main.bounds)
+        window.rootViewController = UIViewController()
+        window.makeKeyAndVisible()
+        
+        return true
+    }
+
+}
