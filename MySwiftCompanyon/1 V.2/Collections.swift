c3e342b470f32e4a7a4fc85ba6dd19026825b296
BerezentesvEvgeny <jonn09@icloud.com>
2022-03-18 15:40:32 +0300

    - Array - Set

---
+++ MySwiftCompanyon/1 V.2/Collections.swift
@@ -0,0 +1,10 @@
+//
+//  Collections.swift
+//  MySwiftCompanyon
+//
+//  Created by Евгений Березенцев on 18.03.2022.
+//
+
+// Array and their functions var someCollection: [String] = []
+
+// Set and their functions  var someCollection: Set<Int> = []
