ae3e4d524e445eb21d144314d84b14264e2d56e3
BerezentesvEvgeny <jonn09@icloud.com>
2022-03-24 20:52:01 +0300

    - Interface

---
+++ MySwiftCompanyon/Swiftbook/Block 2/Interface.swift
@@ -0,0 +1,12 @@
+//
+//  Interface.swift
+//  MySwiftCompanyon
+//
+//  Created by Евгений Березенцев on 24.03.2022.
+//
+
+// viewDidLoad() - еще не известны реальные размеры элементов
+// viewVillAppear() - становятся известны размеры конкретной вью
+
+// UIView -> UIControl -> UISegmentedControl - активные элементы
+// UIView -> UILabel - те с которыми юзеру нельзя напрямую взаимодействовать
