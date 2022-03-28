dbddf2fe50a0f50fc6b585556156fbc76992ef34
BerezentesvEvgeny <jonn09@icloud.com>
2022-03-28 20:50:14 +0300

    - Typecasting

---
+++ MySwiftCompanyon/Swiftbook/Block 2/Typecasting.swift
@@ -0,0 +1,19 @@
+//
+//  Typecasting.swift
+//  MySwiftCompanyon
+//
+//  Created by Евгений Березенцев on 28.03.2022.
+//
+
+// Контроль типа (is проверяет на соответствие типу)
+
+// let pet = getPet(_ pet: Animal)
+// if pet is Dog {
+// print("its a dog")
+// }
+
+// Приведение типов  (as? приводит к типу,при этом делает опционалом,поэтому нужно его извлекать)
+
+//  if let dog = pet as? Dog {
+//    walk(with: Dog)
+//  }
