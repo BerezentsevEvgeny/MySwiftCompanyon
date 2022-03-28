845076c1c0dfc4e430dbdb28b97e71bfb07e0f6e
BerezentesvEvgeny <jonn09@icloud.com>
2022-03-28 18:47:58 +0300

    - DatePicker

--- MySwiftCompanyon/Swiftbook/Block 2/UIElements.swift
+++ MySwiftCompanyon/Swiftbook/Block 2/UIElements.swift
@@ -5,4 +5,25 @@
 //  Created by Евгений Березенцев on 27.03.2022.
 //
 
+// Segmented control
+
+// Label
+
+// Slider
+
+// Text field
+// guard let inputText = textField.text, !inputText.isEmpty { else return )
+// if let _ = Double(inputText) {
+//            print("Wrong format"
+// }
+
+// UIButton
+
+// DateRicker
+
+// dateFormatter.lacale = Locale.current
+// dateFormatter.lacale = Locale(idenitfirer: "ru_Ru" ) Google: ios locale identifiers
+
+// let formater = DateFormatter()
+//formatter.dateStyle = .medium
 
