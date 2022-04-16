8ddeec8f555d9efd08f3339d139ea4232f8390f6
BerezentesvEvgeny <jonn09@icloud.com>
2022-04-16 13:17:07 +0300

    - Clean Swift

---
+++ MySwiftCompanyon/Swiftbook/Block 4/Clean Swift.swift
@@ -0,0 +1,9 @@
+//
+//  Clean Swift.swift
+//  MySwiftCompanyon
+//
+//  Created by Евгений Березенцев on 16.04.2022.
+//
+
+// Циклично связаны viewController -> Interactor -> Presenter
+// сфязь между ними через request -> response -> viewModel(которая возвпащается во вью для отображения)
